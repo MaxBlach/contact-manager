6c1131f46a3542f0e15c503e492c415d631145ce
maximee <max33fun@gmail.com>
2025-06-16 18:04:31 +0200

    add: add contact btn

--- app/contacts/page.tsx
+++ app/contacts/page.tsx
@@ -3,11 +3,14 @@ import { useEffect, useState } from "react";
 import { columns } from "./columns";
 import { DataTable } from "@/components/DataTable";
 import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { PlusIcon } from "lucide-react";
+import { useRouter } from "next/navigation";
 
 const ContactListPage = () => {
   const [search, setSearch] = useState("");
   const [data, setData] = useState([]);
-
+  const router = useRouter();
   const getContacts = async () => {
     const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/contacts?search=${search}`, {
       cache: 'no-store',
@@ -22,7 +25,13 @@ const ContactListPage = () => {
 
   return (
     <div className="w-full flex flex-col gap-4">
-      <Input className="w-1/2" type="text" placeholder="Rechercher (nom, prénom, email, téléphone)" value={search} onChange={(e) => setSearch(e.target.value)}/>
+      <div className="flex justify-between items-center">
+        <Input className="w-1/2" type="text" placeholder="Rechercher (nom, prénom, email, téléphone)" value={search} onChange={(e) => setSearch(e.target.value)}/>
+        <Button onClick={() => router.push("/contacts/new")}>
+          <PlusIcon className="w-4 h-4" />
+          Ajouter un contact
+        </Button>
+      </div>
       <DataTable columns={columns} data={data}/>
     </div>
   );
